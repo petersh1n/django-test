4f1ce65f4e88e1f58f22fa2c2554fb10893e58b1
Peter <abcshin2002@gmail.com>
2024-09-22 00:29:05 +0900

    django start

---
+++ myapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, HttpResponse
+
+topics = [
+{'id':1, 'title':'routing', 'body':'Routing is ..'},
+{'id':2, 'title':'view', 'body':'View is ..'},
+{'id':3, 'title':'Model', 'body':'Model is ..'},
+]
+
+def index(request):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li>,
+        <a href="/read/{topic["id"]}>,
+        {topic["title"]},
+        </a><li>'
+    return HttpResponse,
+('''
+    <html>
+    <body>
+            <h1>Django</h1>
+        <ol>
+            {ol}
+        </ol>
+        <h2>Welcome</h2>
+        Hello, Django
+    </body>
+    </html>
+''')
+def create(request):
+    return HttpResponse('Create')
+
+def read(request, id):
+    return HttpResponse('Read!'+id)
